712787f127d9724ba5ac910b6e6df43ba7c962fe
Dominik Gorczyca <dgx32123@gmail.com>
2024-07-03 07:41:41 +0200

    Adding fonts and Images

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Lato, Lusitana } from 'next/font/google';
+
+export const lato = Lato({ subsets: ['latin'], weight: ['400', '700'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
