67d5ab084674632023395a6f51972a0c4d0d9a72
dominikgorczyca <dgx32123@gmail.com>
2024-07-14 09:06:18 +0200

    Updating Metadata

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -7,6 +7,11 @@ import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages  } from '@/app/lib/data';
 import clsx from 'clsx';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Invoices'
+};
 
 export default async function Page({
     searchParams,
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,24 @@
 import '@/app/ui/global.css';
 import { lato } from '@/app/ui/fonts';
 import { Metadata } from 'next';
- 
+
 export const metadata: Metadata = {
-  title: 'Acme Dashboard',
-  description: 'The official Next.js Course Dashboard, built with App Router.',
-  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+    title: {
+        template: '%s | Acme Dashboard',
+        default: 'Acme Dashboard',
+    },
+    description: 'The official Next.js Course Dashboard, built with App Router.',
+    metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={`${lato.className} antialiased`}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={`${lato.className} antialiased`}>{children}</body>
+        </html>
+    );
 }
